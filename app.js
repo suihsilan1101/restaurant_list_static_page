9a8fa971a00f0a34e22b128551132b6229f4d480
Sui <yufangcheng1101@gmail.com>
2021-06-29 14:36:39 +1000

    feat: add show page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 const port = 3009
 
 app.engine('hbs', exphbs({ defaultLayout: 'main' , extname: '.hbs'}))
@@ -9,7 +10,17 @@ app.set('view engine','hbs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index') //伺服器得到browser的請求而回應
+  res.render('index', { restaurants: restaurantList.results}) //伺服器得到browser的請求而回應
+})
+
+app.get('/restaurants/:restaurant', (req, res) =>{
+  let id = req.params.restaurant
+  console.log(id)
+  let restaurant = restaurantList.results.find(
+    item => item.id.toString() === id
+  )
+  console.log(restaurant)
+  res.render('show', { restaurant: restaurant })
 })
 
 app.listen(port, () =>{
